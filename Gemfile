8203160a532dd0d7a95300c0427a19718967a6de
Cory Forsyth <cory.forsyth@gmail.com>
2011-07-26 17:11:59 -0400

    - initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+gem 'rails', '3.0.9'
+gem 'sqlite3'
+gem "rspec", ">= 2.0.0", :group => :test
+gem "rspec-rails", ">= 2.0.0", :group => :test
+gem "database_cleaner", :group => :test
+gem "mocha", :group => :test
+gem "factory_girl_rails", :group => :test
+gem "rails3-generators"
+gem "jammit"
+gem "haml"
+gem "haml-rails"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sellout::Application.config.secret_token = '6b6123c35bc21cb9b2dedbd815ffc115116f30cec44f3227834db09a43f9a79134a2ecd4b5ba2e3e199a6543d3bf2c47fd4f77c04f0cb55e02caed3df823b317'
